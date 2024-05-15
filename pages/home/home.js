28e6df1a6b9ed93b17ca0ec94a2ef6924183fa8b
ascaryaaa <allaamascarya@gmail.com>
2024-05-15 13:15:11 +0700

    initial commit

---
+++ pages/home/home.js
@@ -0,0 +1,17 @@
+Page({
+  navigateToScreen1() {
+    my.navigateTo({
+      url: '/pages/screen1/screen1'
+    });
+  },
+  navigateToScreen2() {
+    my.navigateTo({
+      url: '/pages/screen2/screen2'
+    });
+  },
+  navigateToScreen3() {
+    my.navigateTo({
+      url: '/pages/screen3/screen3'
+    });
+  }
+});
